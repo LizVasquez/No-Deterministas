12b901288ec641af29e0d37388f141557d3c7e40
alexia <alexiamollo123@gmail.com>
2019-04-25 09:02:45 -0400

    fondoPantalla

---
+++ app/src/main/java/com/example/nodeterministas/Login.java
@@ -0,0 +1,23 @@
+package com.example.nodeterministas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+    public void doLogin(View view){
+        Toast.makeText(this, "Acabas de hacer login!!", Toast.LENGTH_LONG).show();
+        // Creamos un intent para ir a otro activity
+        Intent intent = new Intent(this, menuPrincipalActivity.class);
+        startActivity(intent);
+    }
+    //no aparece el checkbox
+}
