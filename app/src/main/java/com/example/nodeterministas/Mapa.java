148291e4542f7d827891bd67df699286fb1da41b
Raisa <raisa2201silva@gmail.com>
2019-05-08 13:58:28 -0400

    problema de mapas

--- app/src/main/java/com/example/nodeterministas/Mapa.java
+++ app/src/main/java/com/example/nodeterministas/Mapa.java
@@ -3,11 +3,50 @@ package com.example.nodeterministas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static com.google.android.gms.maps.GoogleMap.*;
+
 public class Mapa extends AppCompatActivity {
+
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        this.googleMap.addMarker(new MarkerOptions().title("Marcador 1").position(new LatLng(0, 0)).snippet("Descripcion"));
+
+        this.googleMap.setOnMarkerClickListener(new OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+
+                return false;
+            }
+        });
+
+        this.googleMap.setOnInfoWindowClickListener(new OnInfoWindowClickListener() {
+
+
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+
+            }
+        });
     }
 }
+
