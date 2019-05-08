552648b5575010ecf9ea210c14fec0c15262989e
alexiamollo <alexiamollo123@gmail.com>
2019-05-07 23:37:58 -0400

    Funcionamiento de boton confirmar cambios en ListaAplicacionesInstaladas.class

--- app/src/main/java/com/example/nodeterministas/Activities/ListaAplicacionesInstaladas.java
+++ app/src/main/java/com/example/nodeterministas/Activities/ListaAplicacionesInstaladas.java
@@ -1,32 +1,39 @@
 package com.example.nodeterministas.Activities;
 
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.nodeterministas.AdaptadorRecyclerAplicaciones;
 import com.example.nodeterministas.AdaptadorRecyclerOtrasApp;
+import com.example.nodeterministas.BloquearAplicaciones;
 import com.example.nodeterministas.FuenteOtrasAplicaciones;
 import com.example.nodeterministas.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ListaAplicacionesInstaladas extends AppCompatActivity {
+public class ListaAplicacionesInstaladas extends AppCompatActivity implements View.OnClickListener{
 
     ListView listaAplicaciones;
+    Button confirmarCambios_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_aplicaciones_instaladas);
 
-        String[] array = new String[] {"cat", "dog", "mouse"};
+        //Inicializacion de relacion de componentes
+        confirmarCambios_btn = findViewById(R.id.confirmarcambios_btn);
+        confirmarCambios_btn.setOnClickListener(this);
 
         //get a list of installed apps.
         final PackageManager pm = getPackageManager();
@@ -56,5 +63,13 @@ public class ListaAplicacionesInstaladas extends AppCompatActivity {
         otrasAppRv.setAdapter(new AdaptadorRecyclerOtrasApp(listaOtrasApp));
         otrasAppRv.setLayoutManager(layout);
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.confirmarcambios_btn){
+            Intent intent = new Intent(confirmarCambios_btn.getContext(), BloquearAplicaciones.class);
+            startActivity(intent);
+        }
     }
 }
