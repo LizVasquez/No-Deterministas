2333550a8dda1bb314fe8fd414f63b6b6c5b80bc
Raisa <raisa2201silva@gmail.com>
2019-05-09 10:59:50 -0400

    ubicaiones de las discotecas

--- app/src/main/java/com/example/nodeterministas/Mapa.java
+++ app/src/main/java/com/example/nodeterministas/Mapa.java
@@ -30,9 +30,12 @@ public class Mapa extends AppCompatActivity implements OnMapReadyCallback {
         this.googleMap = googleMap;
 
         this.googleMap.addMarker(new MarkerOptions().title("La Paz").position(new LatLng(-16.5, -68.1500015)).snippet("Ciudad"));
-        this.googleMap.addMarker(new MarkerOptions().title("Santa Cruz").position(new LatLng(-17.7862892	, -63.1811714)).snippet("Ciudad"));
-        this.googleMap.addMarker(new MarkerOptions().title("Santa Cruz").position(new LatLng(-17.7862892	, -63.1811714)).snippet("Ciudad"));
-
+        this.googleMap.addMarker(new MarkerOptions().title("Beyond").position(new LatLng(-16.5752060, -68.12)).snippet("Discoteca"));
+        this.googleMap.addMarker(new MarkerOptions().title("Legend").position(new LatLng(-16.575195,-68.12)).snippet("Discoteca"));
+        this.googleMap.addMarker(new MarkerOptions().title("London").position(new LatLng(-16.575206,-68 )).snippet("Discoteca"));
+        this.googleMap.addMarker(new MarkerOptions().title("Gitana").position(new LatLng(-16.5424351,-68.09)).snippet("Discoteca"));
+        this.googleMap.addMarker(new MarkerOptions().title("Glam").position(new LatLng(-16.5095712,-68.1308)).snippet("Discoteca"));
+        this.googleMap.addMarker(new MarkerOptions().title("Pa Goza").position(new LatLng(-16.575206,-68 )).snippet("Discoteca"));
 
 
 
