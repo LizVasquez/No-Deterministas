0f0d78708855d828f8fcff62d439bedd4e0ca4bb
Raisa <raisa2201silva@gmail.com>
2019-05-09 08:59:15 -0400

    mapas con ubicación

--- app/src/main/java/com/example/nodeterministas/Busquemos.java
+++ app/src/main/java/com/example/nodeterministas/Busquemos.java
@@ -1,7 +1,9 @@
 package com.example.nodeterministas;
 
+import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +15,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-    public class Busquemos extends AppCompatActivity {
+import pub.devrel.easypermissions.EasyPermissions;
+import pub.devrel.easypermissions.PermissionRequest;
+
+public class Busquemos extends AppCompatActivity {
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +35,24 @@ import android.widget.Toast;
                 }
             });
 
+// Request Permissions
+            String[] perms = {Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+            EasyPermissions.requestPermissions(
+                    new PermissionRequest.Builder(this, 123, perms)
+                            .setPositiveButtonText("ACEPTAR")
+                            .setNegativeButtonText("CANCELAR")
+                            .build());
+
         }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+        }
+
     }
 
 
--- app/src/main/java/com/example/nodeterministas/Mapa.java
+++ app/src/main/java/com/example/nodeterministas/Mapa.java
@@ -3,11 +3,49 @@ package com.example.nodeterministas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Mapa extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class Mapa extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+       mapFragment.getMapAsync (  this);
     }
+
+
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        this.googleMap.addMarker(new MarkerOptions().title("Marcador 1").position(new LatLng(0, 0)).snippet("Descripcion"));
+
+        this.googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+
+                return false;
+            }
+        });
+
+        this.googleMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+
+
+            }
+        });
+    }
+
 }
